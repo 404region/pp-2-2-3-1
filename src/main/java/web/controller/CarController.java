15a1d8d7652a96215d16370db9296d0202785b89
Ann S <404region@gmail.com>
2023-10-24 17:48:29 +0300

    Исправлены замечания ментора 3

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -6,15 +6,13 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
-import web.service.CarServiceImpl;
-
-import java.util.ArrayList;
+import web.service.CarService;
 import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 public class CarController {
-    private final CarServiceImpl carServiceImpl;
+    private final CarService carServiceImpl;
 
     @GetMapping(value = "/cars")
     public String printWelcome(@RequestParam(value = "count", required = false, defaultValue = "0") Integer count, ModelMap model) {
