88b3d3946dc225df2605f518b45becaf77509a8a
Ann S <404region@gmail.com>
2023-10-25 09:28:43 +0300

    Исправлены замечания ментора 4

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -5,20 +5,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.model.Car;
 import web.service.CarService;
-import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 public class CarController {
-    private final CarService carServiceImpl;
+    private final CarService carService;
 
     @GetMapping(value = "/cars")
     public String printWelcome(@RequestParam(value = "count", required = false, defaultValue = "0") Integer count, ModelMap model) {
 
-        List<Car> carsList = carServiceImpl.getAllCars(count);
-        model.addAttribute("carsList", carsList);
+        model.addAttribute("carsList", carService.getAllCars(count));
         return "cars";
     }
 }
